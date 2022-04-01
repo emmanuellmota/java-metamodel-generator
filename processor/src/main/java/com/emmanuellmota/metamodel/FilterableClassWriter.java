ffc7412d0d33efeebadacaef5366d2d6e042ae0e
Emmanuel Mota <emmanuellmota@gmail.com>
2022-03-31 22:33:42 -0300

    Completely remove third-party annotations

--- processor/src/main/java/com/emmanuellmota/metamodel/FilterableClassWriter.java
+++ processor/src/main/java/com/emmanuellmota/metamodel/FilterableClassWriter.java
@@ -13,6 +13,7 @@ import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
@@ -29,17 +30,21 @@ class FilterableClassWriter {
     private final TypeElement beanType;
     private final ClassModel  classModel;
     private final String      metaClassName;
-    private final String      filterClassName;
+    private final ClassName   filterClassName;
 
     /**
      * Initialize class with {@link TypeElement} and {@link ClassModel} containing attributes.
      * @param beanType the bean class.
      * @param classModel attribute informations about the bean class.
      */
-    FilterableClassWriter(TypeElement beanType, ClassModel classModel, String filterClassName) {
+    FilterableClassWriter(TypeElement beanType, ClassModel classModel, String packageName) {
         this.beanType = beanType;
         this.classModel = classModel;
-        this.filterClassName = filterClassName;
+        
+        String[] classArr = packageName.split("\\.");
+        this.filterClassName = ClassName.get(String.join(".", Arrays.copyOf(classArr, classArr.length - 1)),
+                classArr[classArr.length - 1]);
+
         metaClassName = beanType.getSimpleName() + SUFFIX;
     }
 
@@ -48,7 +53,7 @@ class FilterableClassWriter {
      * @throws IOException if source file cannot be written.
      */
     void invoke() throws IOException {
-        TypeSpec.Builder classBuilder = TypeSpec.classBuilder(metaClassName)
+        /* TypeSpec.Builder classBuilder = TypeSpec.classBuilder(metaClassName)
                                                 .addModifiers(Modifier.PUBLIC)
                                                 .addAnnotation(Data.class);
         classModel.attributes().forEach((name, type) -> classBuilder.addField(createFieldSpec(name, type)));
@@ -56,6 +61,38 @@ class FilterableClassWriter {
         JavaFile javaFile = JavaFile.builder(ClassName.get(beanType).packageName(), classBuilder.build()).indent("    ")
                                     .addFileComment("Generated code. Do not modify!")
                                     .build();
+        javaFile.writeTo(classModel.getEnvironment().getFiler()); */
+
+
+
+
+        TypeSpec.Builder classBuilder = TypeSpec.classBuilder(metaClassName)
+                .addModifiers(Modifier.PUBLIC);
+        classModel.attributes().forEach((name, type) -> classBuilder.addField(createFieldSpec(name, type)));
+
+        classBuilder.addMethod(MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC)
+                .build());
+
+        classModel.attributes()
+                .forEach((name, type) -> classBuilder.addMethod(
+                        MethodSpec.methodBuilder("get" + name.substring(0, 1).toUpperCase() + name.substring(1))
+                                .addModifiers(Modifier.PUBLIC)
+                                .returns(filterClassName)
+                                .addStatement("return $L", name)
+                                .build()));
+
+        classModel.attributes()
+                .forEach((name, type) -> classBuilder.addMethod(
+                        MethodSpec.methodBuilder("set" + name.substring(0, 1).toUpperCase() + name.substring(1))
+                                .addModifiers(Modifier.PUBLIC)
+                                .returns(void.class)
+                                .addParameter(ParameterizedTypeName.get(filterClassName, Utils.getAttributeTypeName(type)), name)
+                                .addStatement("this.$1N = $1N", name)
+                                .build()));
+
+        JavaFile javaFile = JavaFile.builder(ClassName.get(beanType).packageName(), classBuilder.build()).indent("    ")
+                .build();
         javaFile.writeTo(classModel.getEnvironment().getFiler());
     }
 
@@ -67,8 +104,7 @@ class FilterableClassWriter {
 
     private ParameterizedTypeName declarationTypeName(AttributeInfo info) {
         final TypeName attributeTypeName = Utils.getAttributeTypeName(info);
-        String[] classArr = filterClassName.split("\\.");
-        return ParameterizedTypeName.get(ClassName.get(String.join(".", Arrays.copyOf(classArr, classArr.length - 1)), classArr[classArr.length - 1]), attributeTypeName);
+        return ParameterizedTypeName.get(filterClassName, attributeTypeName);
     }
 
 }
--- processor/src/main/java/com/emmanuellmota/metamodel/OrderableClassWriter.java
+++ processor/src/main/java/com/emmanuellmota/metamodel/OrderableClassWriter.java
@@ -11,6 +11,7 @@ import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 
 import lombok.Data;
@@ -20,44 +21,71 @@ import lombok.Data;
  */
 class OrderableClassWriter {
 
-    private static final String SUFFIX           = "__Orderable";
+    private static final String SUFFIX = "__Orderable";
 
     private final TypeElement beanType;
-    private final ClassModel  classModel;
-    private final String      metaClassName;
-    private final String      orderClassName;
+    private final ClassModel classModel;
+    private final String metaClassName;
+    private final ClassName orderClassName;
 
     /**
-     * Initialize class with {@link TypeElement} and {@link ClassModel} containing attributes.
-     * @param beanType the bean class.
+     * Initialize class with {@link TypeElement} and {@link ClassModel} containing
+     * attributes.
+     * 
+     * @param beanType   the bean class.
      * @param classModel attribute informations about the bean class.
      */
-    OrderableClassWriter(TypeElement beanType, ClassModel classModel, String orderClassName) {
+    OrderableClassWriter(TypeElement beanType, ClassModel classModel, String packageName) {
         this.beanType = beanType;
         this.classModel = classModel;
-        this.orderClassName = orderClassName;
+
+        String[] classArr = packageName.split("\\.");
+        this.orderClassName = ClassName.get(String.join(".", Arrays.copyOf(classArr, classArr.length - 1)),
+                classArr[classArr.length - 1]);
+
         metaClassName = beanType.getSimpleName() + SUFFIX;
     }
 
     /**
      * Create the Metamodel class.
+     * 
      * @throws IOException if source file cannot be written.
      */
     void invoke() throws IOException {
         TypeSpec.Builder classBuilder = TypeSpec.classBuilder(metaClassName)
-                                                .addModifiers(Modifier.PUBLIC)
-                                                .addAnnotation(Data.class);
+                .addModifiers(Modifier.PUBLIC);
         classModel.attributes().forEach((name, type) -> classBuilder.addField(createFieldSpec(name, type)));
 
+        classBuilder.addMethod(MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC)
+                .build());
+
+        classModel.attributes()
+                .forEach((name, type) -> classBuilder.addMethod(
+                        MethodSpec.methodBuilder("get" + name.substring(0, 1).toUpperCase() + name.substring(1))
+                                .addModifiers(Modifier.PUBLIC)
+                                .returns(orderClassName)
+                                .addStatement("return $L", name)
+                                .build()));
+
+        classModel.attributes()
+                .forEach((name, type) -> classBuilder.addMethod(
+                        MethodSpec.methodBuilder("set" + name.substring(0, 1).toUpperCase() + name.substring(1))
+                                .addModifiers(Modifier.PUBLIC)
+                                .returns(void.class)
+                                .addParameter(orderClassName, name)
+                                .addStatement("this.$1N = $1N", name)
+                                .build()));
+
         JavaFile javaFile = JavaFile.builder(ClassName.get(beanType).packageName(), classBuilder.build()).indent("    ")
-                                    .build();
+                .build();
         javaFile.writeTo(classModel.getEnvironment().getFiler());
     }
 
     private FieldSpec createFieldSpec(String attributeName, AttributeInfo info) {
-        String[] classArr = orderClassName.split("\\.");
-        return FieldSpec.builder(ClassName.get(String.join(".", Arrays.copyOf(classArr, classArr.length - 1)), classArr[classArr.length - 1]), attributeName)
-                        .addModifiers(Modifier.PRIVATE).build();
+        return FieldSpec
+                .builder(orderClassName, attributeName)
+                .addModifiers(Modifier.PRIVATE).build();
     }
 
 }
